9f48ab5cc27a2fe5a1b64485e8f358670770dccf
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-16 19:45:56 +0100

    Implementazione voti li arrotondo o per eccesso o per difetto e le trasformo in 5 stelle piene/vuote

--- resources/views/layouts/MainLayout.blade.php
+++ resources/views/layouts/MainLayout.blade.php
@@ -4,10 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Movie | @yield('page-title', 'Standard Title')</title>
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Font -->
+            <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <!-- Font -->
+        <!-- Fontawesome CDN -->
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- Fontawesome CDN -->
         <!-- Styles -->
-        @vite('resources/js/app.js')
+            @vite('resources/js/app.js')
     </head>
     <body>
         @include('partials.Header')
